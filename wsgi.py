d5d8f17f1e7ec1b32a491c80f743cbf350c746b8
Gianluca Rigoletti <gianluca.rigoletti@cern.ch>
2020-10-04 20:54:16 +0200

    Add basic code

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,25 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/upload', methods=['POST'])
+def upload():
+    print('Got request')
+    CODE = os.getenv('CODE')
+    file = request.data
+    code = request.headers['Code']
+    if code == os.getenv('CODE'):
+        filepath = Path(os.getenv('FOLDER')) / f'{round(time() * 1000)}.jpeg'
+        with open(filepath, 'wb+') as f:
+            f.write(file)
+        print(f'Saved as {file}')
+        return '', 200
+
+    print(f'Request not matching code: received: {CODE} set: {code}')
+    return '', 401
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return 'Hello', 200
 
 if __name__ == "__main__":
     application.run()
